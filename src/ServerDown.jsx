10efa458ae782cf96c57998c315328ff9c5be208
Nathan <nathan.froeh@gmail.com>
2019-12-18 11:05:20 -0700

    Add ServerDown component

---
+++ src/ServerDown.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServerDown() {
+  return (
+    <div className='server-down'>
+      <pre>Error: Database servers are down</pre>
+    </div>
+  )
+}
