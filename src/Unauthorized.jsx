4fb27c4d8c2830bd939974520faa40239d94e364
Nathan <nathan.froeh@gmail.com>
2019-12-18 11:04:11 -0700

    Add Unauthorized component

---
+++ src/Unauthorized.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Unauthorized() {
+  return (
+    <div className='unauthorized'>
+      <pre>Error: External API call authorization failed</pre>
+    </div>
+  )
+}
