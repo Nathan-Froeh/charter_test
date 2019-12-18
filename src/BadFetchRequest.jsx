47a8350c345c478e48ebeff00c5849139250e992
Nathan <nathan.froeh@gmail.com>
2019-12-18 11:02:48 -0700

    Add BadFetchRequest component

---
+++ src/BadFetchRequest.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BadFetchRequest() {
+  return (
+    <div className='bad-fetch-request'>
+      <pre>Error: RestAPI call made and couldn't fetch any data</pre>
+    </div>
+  )
+}
