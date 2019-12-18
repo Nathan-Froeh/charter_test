5dd2788b94d85f20b4dae279f8a14eb5ae186950
Nathan <nathan.froeh@gmail.com>
2019-12-18 11:30:53 -0700

    Add issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Search from './Search';
 
 function App() {
+  const issues = [
+    "Issue#1: Database Connectivity Failure",
+    "Issue#2: RestAPI call made and couldn't fetch any data",
+    "Issue#3: External API call authorization failed",
+    "Issue#4: Database servers are down",
+    "Issue#5: UI rendering error"
+  ]
   return (
     <div className="App">
-      
+      <Search issues={issues}/>
     </div>
   );
 }
