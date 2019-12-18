64669892b007e502a8ce09e1a5f12f7e25e58ba7
Nathan <nathan.froeh@gmail.com>
2019-12-18 11:00:42 -0700

    Add DatabaseConnection component

---
+++ src/DatabaseConnection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DatabaseConnection() {
+  return (
+    <div className='database-connection'>
+      <pre>Error: Database Connectivity Failure</pre>
+    </div>
+  )
+}
