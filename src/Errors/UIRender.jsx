5fafd1e9bd1460716d0453fb37537cc43cd19217
Nathan <nathan.froeh@gmail.com>
2019-12-20 13:10:56 -0700

    Add back btn, UIRender

--- src/Errors/UIRender.jsx
+++ src/Errors/UIRender.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function UIRender() {
   return (
     <div className='ui-render'>
       <pre>Error: UI rendering error</pre>
+      <Link to={'/'}>
+        <button>Back</button>
+      </Link>
     </div>
   )
 }
