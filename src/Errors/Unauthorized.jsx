d73def2726e631ea7c9618046f7336a6a9688f7e
Nathan <nathan.froeh@gmail.com>
2019-12-20 13:11:26 -0700

    Add back btn, Unauthorized

--- src/Errors/Unauthorized.jsx
+++ src/Errors/Unauthorized.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Unauthorized() {
   return (
     <div className='unauthorized'>
       <pre>Error: External API call authorization failed</pre>
+      <Link to={'/'}>
+        <button>Back</button>
+      </Link>
     </div>
   )
 }
