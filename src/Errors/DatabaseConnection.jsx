cd694975dd08ed9d5ddb19755c39ef908a9d3693
Nathan <nathan.froeh@gmail.com>
2019-12-20 13:11:58 -0700

    Add back btn, DatabaseConnection

--- src/Errors/DatabaseConnection.jsx
+++ src/Errors/DatabaseConnection.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function DatabaseConnection() {
   return (
     <div className='database-connection'>
       <pre>Error: Database Connectivity Failure</pre>
+      <Link to={'/'}>
+        <button>Back</button>
+      </Link>
     </div>
   )
 }
