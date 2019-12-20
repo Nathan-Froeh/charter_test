1d4df552266de0cc70f7e5ac6d049567223c27ca
Nathan <nathan.froeh@gmail.com>
2019-12-20 10:09:35 -0700

    Add search function

--- src/Search.jsx
+++ src/Search.jsx
@@ -8,12 +8,22 @@ export class Search extends Component {
     }
   }
 
+  filterList = (e) => {
+    let items = this.props.issues;
+    items = items.filter((item) => {
+      return item.error.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+    });
+    if(e.target.value){
+      this.setState({matchingIssues: items});
+    } else {this.setState({matchingIssues: []})}
+  }
+
   render() {
     return (
       <form className='search'>
         <h1>React Demo</h1>
         <h3>Start typing to search issues!</h3>
-        <input type="search"/>
+        <input type="search" onChange={this.filterList}/>
       </form>
     )
   }
